fdf74c29d58ed5e8f2c292b972adfc5dcba94518
chucdoan <chucdb2003@gmail.com>
2024-10-19 23:34:00 +0700

    ad

--- Backend-filmhub/database/seeders/DatabaseSeeder.php
+++ Backend-filmhub/database/seeders/DatabaseSeeder.php
@@ -12,12 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         $this->call([
-            TheaterSeeder::class,
-            ShiftSeeder::class,
-            RoomSeeder::class,
-        ]);
-
-       
+        
     }
 }
