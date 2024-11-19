237d625a68cfbb1f6cdbcc898d9bd2f6129578dd
TK-NNH <vipdktq@gmail.com>
2024-11-19 21:18:42 +0700

    Thêm db bảng vouchers

--- Backend-filmhub/database/migrations/2024_11_18_033107_create_vourchers_table.php
+++ Backend-filmhub/database/migrations/2024_11_18_033107_create_vourchers_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('vourchers', function (Blueprint $table) {
             $table->id();
+            $table->integer('vourcher_code');
+            $table->integer('vourcher_price');
             $table->timestamps();
         });
     }
